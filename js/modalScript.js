59876724c20b5f9dc530076edad4c67e8d17a449
Bermelio <facundoperalta258@gmail.com>
2024-11-27 20:50:47 -0300

    terminado el html y el css solo queda la logica de 0, buenas practicas

--- js/modalScript.js
+++ js/modalScript.js
@@ -1,5 +1,20 @@
 // Getting modal from html
 
-const modal = document.getElementById("modalContainer");
+const modal = document.getElementById("modal");
 const triggerModal = document.getElementById("triggerModal");
 
+const span = document.getElementsByClassName("close")[0];
+
+triggerModal.onclick = function(){
+    modal.style.display = "block";
+}
+
+span.onclick = function(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
